6cd2d12686acca1185089653502703177a33828b
Joellc <78988456+Joellc@users.noreply.github.com>
2021-12-30 02:01:54 -0500

    Delete Challengesofka directory

--- Challengesofka/src/main/java/co/com/sofka/Challengesofka/repositorio/JugadorRepositorio.java
+++
@@ -1,9 +0,0 @@
-package co.com.sofka.Challengesofka.repositorio;
-
-import co.com.sofka.Challengesofka.model.Jugador;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-public interface JugadorRepositorio extends CrudRepository <Jugador, Integer>{
-    List<Jugador> findByNombre(String nombre);
-}
