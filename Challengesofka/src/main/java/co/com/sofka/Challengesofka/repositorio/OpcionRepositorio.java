29879e9fea4ff4d13c03226e080b84b4fef02957
Joellc <78988456+Joellc@users.noreply.github.com>
2021-12-30 04:24:07 -0500

    Delete Challengesofka directory

--- Challengesofka/src/main/java/co/com/sofka/Challengesofka/repositorio/OpcionRepositorio.java
+++
@@ -1,8 +0,0 @@
-package co.com.sofka.Challengesofka.repositorio;
-
-import co.com.sofka.Challengesofka.model.Opcion;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OpcionRepositorio extends CrudRepository <Opcion, Integer> {
-
-}
